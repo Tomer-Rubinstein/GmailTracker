1000c7fa5754f7407ae1ccaef3029925304f1bfa
TomeR <tomerrub11@gmail.com>
2022-11-19 13:09:27 +0200

    implemented mongodb

--- webserver/app.py
+++ webserver/app.py
@@ -1,16 +1,35 @@
 from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
+import pymongo
+import mongoenv
 
+conn_str = mongoenv.mongo_connstr_gmailutils
+client = pymongo.MongoClient(conn_str, serverSelectionTimeoutMS=5000)
+db = client["GmailUtils"]
+mails_collection = db["mails"]
 
 class NewMailPostData(BaseModel):
   mid: str
 
 
 app = FastAPI()
-dummyDb = [] # TODO implement postgresql
+
+app.add_middleware(
+  CORSMiddleware,
+  allow_origins=["*"],
+  allow_credentials=True,
+  allow_methods=["*"],
+  allow_headers=["*"]
+)
 
 
-@app.post("/newMail/")
+@app.post("/newMail")
 async def newMail(data: NewMailPostData):
-  dummyDb.append(data["mid"]) 
-  print(dummyDb)
+  print(data)
+  # mails_collection.insert_one({
+  #   "mid": data.mid,
+  #   "hasOpened": False,
+  #   "openedTimestamp": 0,
+  # })
