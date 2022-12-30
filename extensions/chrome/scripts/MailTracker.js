333ae747dd0d51fd0e22d9c19f0ac9e38002e417
TomeR <tomerrub11@gmail.com>
2022-12-30 20:51:02 +0200

    program can now respond to DOM changes in the text area of each email & can handle multiple email tracking sessions

--- extensions/chrome/scripts/MailTracker.js
+++ extensions/chrome/scripts/MailTracker.js
@@ -1,7 +1,61 @@
 
 
-var trackBtnEnabled = false;
-var HOST = "https://a3fb-2a10-8012-19-cb86-d86c-21bf-33b7-5da2.eu.ngrok.io";
+var HOST = "https://189d-2a10-8012-d-dcc3-1d33-8c1a-793b-df84.eu.ngrok.io";
+
+(async function main(){
+  // get status
+  window.addEventListener('popstate', getEmailStatus)
+
+  // add 'click' event listener to the send button when composing a new gmail.
+  const sendBtnDiv = await waitForElm('[class="dC"]');
+  sendBtnDiv.firstChild.addEventListener('click', sendNewEmail);
+
+  var currentTextArea;
+  /* observe for DOM changes and wait for the UI elements: email toolbar & text area
+     to appear in the "compose email" dialog. */
+  new MutationObserver(async () => {
+    await waitForAllElem('[g_editable="true"][role="textbox"][contenteditable="true"]').then((elemList) => {
+      currentTextArea = elemList[elemList.length-1];
+      if (currentTextArea === undefined) return;
+
+      /* if all email's text area are getting deleted (for instance, CTRL+A & backspace & another backspace)
+         will result in the div of the text area to remove EVERY single children instance */
+      /* thus, it could remove the <img/> payload even though the trackBtn is enabled.
+         so as a solution: listen for those text area changes and add the <img/> if required */
+      new MutationObserver((mutations, obs) => {
+        obs.disconnect();
+
+        document.querySelectorAll('[id="gmailutils_trackbtn"]').forEach(async (trackBtn) => {
+          var correspondingTextArea = document.getElementById(trackBtn.getAttribute("gmailutils_textAreaId"));
+          var mid = await getMid();
+          if (    correspondingTextArea !== undefined
+              &&  correspondingTextArea !== null
+              &&  correspondingTextArea.id === currentTextArea.id
+              &&  trackBtn.getAttribute("isClicked") === 'true'
+              &&  correspondingTextArea.querySelector('[id="gmailutils_img"]') === null    ) {
+            /* tracking is enabled but <img/> doesn't appear in the text area of the email,
+               so add a new <img/> payload */
+            console.log('yessir')
+            correspondingTextArea.appendChild(createImgPayload(mid));
+          }
+        });
+      }).observe(currentTextArea, {childList: true, subtree: true});
+
+
+    }).then(async () => {
+      await waitForAllElem('[class="gU a0z"]').then((elemList) => {
+        var currentLowerDiv = elemList[elemList.length-1];
+        if (currentLowerDiv !== undefined && currentLowerDiv.querySelector('[id="gmailutils_trackbtn"]') === null) {    
+          /* appending trackbtn to the lower toolbar of the email compose dialog */
+          console.log("creating with: ", currentLowerDiv);
+          currentLowerDiv.appendChild(createTrackButton(currentTextArea));
+        }
+      });
+    });
+  }).observe(document, {subtree: true, childList: true});
+
+})();
+
 
 async function HmacSHA256(message, secret) {
   const enc = new TextEncoder("utf-8");
@@ -28,6 +82,7 @@ async function HmacSHA256(message, secret) {
 function getUserEmail(){
   // TODO: class name changes everyday dilemma
   // suggestion: ask users for their email
+  //             or find other ways to parse user email address 
 
   // const profileDiv = await waitForElm('[class="gb_zf gb_Xa gb_mg gb_f"]');
   // const re_match_email = /\(([^\)]+)\)/
@@ -51,10 +106,8 @@ async function getMid(){
   });
 }
 
-/*
-sendNewEmail() is responsible for sending a new mid to the webserver.
-Invoked when the "send" button is clicked when composing a new gmail.
-*/
+/* sendNewEmail() is responsible for sending a new mid to the webserver.
+Invoked when the "send" button is clicked when composing a new gmail. */
 async function sendNewEmail(){
   if (!trackBtnEnabled) return;
 
@@ -81,29 +134,6 @@ async function sendNewEmail(){
 }
 
 
-async function trackBtnClicked(){
-  trackBtnEnabled = !trackBtnEnabled;
-
-  const trackBtn = document.getElementById("gmailutils_trackbtn");
-  trackBtn.innerText = (trackBtnEnabled) ? "Track ✔️" : "Track ❌";
-
-  if (trackBtnEnabled) {
-    await getMid().then((mid) => {
-      // inject <img/> payload to email content
-      var imgPayload = document.createElement("img");
-      imgPayload.src = HOST + "/read?mid=" + mid;
-      imgPayload.setAttribute("mid", mid);
-      imgPayload.id = "gmailutils_img";
-      imgPayload.style.display = 'none';
-      
-      document.getElementById(":ri").appendChild(imgPayload);
-    });
-  }
-  else if ((img = document.getElementById("gmailutils_img")) !== undefined) {
-    img.remove();
-  }
-
-}
 
 function getEmailStatus(){
   const imgPayload = this.document.querySelector('[id*=gmailutils_img]');
@@ -122,8 +152,8 @@ function getEmailStatus(){
       },
       body: JSON.stringify({"mid": mid})
     })
-    .then((resp) => resp.json())
-    .then((data) => {
+    .then(resp => resp.json())
+    .then(data => {
       var date = new Date(data.lastOpened * 1000);
       var seenText = document.createElement("p");
 
@@ -136,45 +166,12 @@ function getEmailStatus(){
   }
 }
 
-(async function main(){
-  // get status
-  window.addEventListener('popstate', getEmailStatus)
-
-  // add 'click' event listener to the send button when composing a new gmail.
-  const sendBtnDiv = await waitForElm('[class="dC"]');
-  sendBtnDiv.firstChild.addEventListener('click', sendNewEmail);
-
-  await waitForElm('[class="gU a0z"]').then((lowerDiv) => {
-    var trackBtn = document.createElement("button");
-    trackBtn.innerText = "Track ❌"
-    trackBtn.id = "gmailutils_trackbtn";
-    trackBtn.onclick = trackBtnClicked;
-    trackBtn.style = `
-      box-shadow: 1px 2px 5px gray;
-      border: none; 
-      border-radius: 5px; 
-      width: 80px; 
-      height: 25px;
-      font-family: "Century Gothic",Verdana,sans-serif;
-      background: #007FFF;
-      color: white;
-      cursor: pointer;
-      margin-right: 5px;
-      margin-left: 5px;
-    `
-
-    lowerDiv.appendChild(trackBtn);
-  });
-
-})();
-
 
 function waitForElm(attr) {
   return new Promise(resolve => {
     // watch for changes in the DOM tree
     const observer = new MutationObserver(mutations => {
       if (document.querySelector(attr)) {
-        // resolve([...document.querySelectorAll(attr)].map(item => item.firstChild));
         resolve(document.querySelector(attr))
         observer.disconnect();
       }
@@ -187,3 +184,86 @@ function waitForElm(attr) {
   });
 }
 
+
+function waitForAllElem(attr){
+  return new Promise(resolve => {
+    // watch for changes in the DOM tree
+    const observer = new MutationObserver(mutations => {
+      if (document.querySelectorAll(attr)) {
+        resolve([...document.querySelectorAll(attr)]);
+        observer.disconnect();
+      }
+    });
+
+    observer.observe(document.body, {
+      childList: true,
+      subtree: true
+    });
+  });
+}
+
+
+function createTrackButton(currentTextArea){
+  /* creating the track button */
+  var trackBtn = document.createElement("button");
+  trackBtn.innerText = "Track ❌";
+  trackBtn.id = `gmailutils_trackbtn`;
+  trackBtn.setAttribute("isClicked", 'false');
+  trackBtn.setAttribute("gmailutils_textAreaId", currentTextArea.id);
+  trackBtn.style = `
+    box-shadow: 1px 2px 5px gray;
+    border: none;
+    border-radius: 5px;
+    width: 80px;
+    height: 25px;
+    font-family: "Century Gothic",Verdana,sans-serif;
+    background: #007FFF;
+    color: white;
+    cursor: pointer;
+    margin-right: 5px;
+    margin-left: 5px;
+  `;
+
+  /* on clicked, append to the CORRESPONDING text area the <img/> payload
+     or remove it if clicked again */
+  trackBtn.onclick = async () => {
+    /* updating the state of the trackbtn (clicked or not) */
+    var trackBtnEnabled = (trackBtn.getAttribute("isClicked") === 'true') ? false : true;
+    trackBtn.setAttribute("isClicked", trackBtnEnabled);
+    trackBtn.innerText = (trackBtnEnabled) ? "Track ✔️" : "Track ❌";
+  
+    var textArea = document.getElementById(trackBtn.getAttribute("gmailutils_textAreaId"));
+    console.log("first: ", trackBtn.getAttribute("gmailutils_textAreaId"))
+    if (textArea === null) return;
+    console.log("here: ", textArea.id);
+
+    if (trackBtnEnabled) {
+      await getMid().then((mid) => {
+        // create & append <img/> payload to the CORRESPONDING text area of the email
+        var imgPayload = createImgPayload(mid);
+
+        document.getElementById(
+          trackBtn.getAttribute("gmailutils_textAreaId")
+        ).appendChild(imgPayload);
+      });
+    } else {
+      /* trackBtnEnabled is false so remove the <img/> payload if exists
+         if it exists in the corresponding email's text area */
+      var imgPayload = textArea.querySelector('[id="gmailutils_img"]');
+      imgPayload.remove();
+    }
+  };
+
+  return trackBtn;
+}
+
+
+function createImgPayload(mid) {
+  var imgPayload = document.createElement("img");
+  imgPayload.src = HOST + "/read?mid=" + mid;
+  imgPayload.setAttribute("mid", mid);
+  imgPayload.id = "gmailutils_img";
+  imgPayload.style.display = 'none';
+
+  return imgPayload;
+}
